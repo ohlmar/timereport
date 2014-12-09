16fbf7160e39cc8f3997f3fab9b1e5f950789d8d
ohlmar <ohlmar88@gmail.com>
2014-12-09 14:32:01 +0100

    Tog bort lite skräp

--- Tidrapport/Scripts/app/datepickercontroller.js
+++ Tidrapport/Scripts/app/datepickercontroller.js
@@ -8,25 +8,4 @@
         $scope.dt = null;
     };
 
-    // Disable weekend selection
-    $scope.disabled = function (date, mode) {
-        return (mode === 'day' && (date.getDay() === 0 || date.getDay() === 6));
-    };
-
-    $scope.toggleMin = function () {
-        $scope.minDate = $scope.minDate ? null : new Date();
-    };
-    $scope.toggleMin();
-
-    $scope.open = function ($event) {
-        $event.preventDefault();
-        $event.stopPropagation();
-
-        $scope.opened = true;
-    };
-
-    $scope.dateOptions = {
-        formatYear: 'yy',
-        startingDay: 1
-    };
 });
