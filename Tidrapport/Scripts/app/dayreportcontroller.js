8dd0ca7366363006158e467d92af15f1befbb1f4
ohlmar <ohlmar88@gmail.com>
2014-12-09 16:40:23 +0100

    Timepickers

---
+++ Tidrapport/Scripts/app/dayreportcontroller.js
@@ -0,0 +1,26 @@
+﻿angular.module('timeReport').controller('dayreportcontroller', function ($scope, $log) {
+
+    $scope.starttime = moment().hours(08).minute(00);
+    $scope.lunchstarttime = moment().hours(11).minute(30);
+    $scope.lunchendtime = moment().hours(12).minute(00);
+    $scope.endtime = moment().hours(16).minute(30);
+    $scope.totaltime = "";
+
+    $scope.hstep = 1;
+    $scope.mstep = 1;
+
+    $scope.ismeridian = false;
+
+    $scope.changed = function () {
+        calcTotalTime();
+    };
+
+    var calcTotalTime = function() {
+        var beforeLunch = moment($scope.lunchstarttime).diff(moment($scope.starttime));
+        var afterLunch = moment($scope.endtime).diff(moment($scope.lunchendtime));
+
+        $scope.totaltime = moment.duration(beforeLunch + afterLunch).hours() + ":" + moment.duration(beforeLunch + afterLunch).minutes();
+    }
+    calcTotalTime();
+
+});
